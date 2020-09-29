9d30daf5cfe00017dcf836a04479441fce7c8bd5
Jain Thomas <jtlearncloud1@gmail.com>
2020-09-28 19:40:51 -0500

    terraform provider

---
+++ provider.tf
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:1391f5e55dbb998fb68d7072926525a4688b75407b84e5b6d1f799948582c1af
+size 154
---
+++ variables.tf
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:e762300dc1e0cf383cbfcab11267fbca95a638a4bb2eef5160c10f8d4fd73dd4
+size 110

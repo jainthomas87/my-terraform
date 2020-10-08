d96f2fb20c50ca2e81321dd057a742f8e8e6f0f8
Jain Thomas <jtlearncloud1@gmail.com>
2020-10-07 22:40:27 -0500

    deleted

--- main.tf
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:56619d585a948549286e0aa68876813518090cf10210e2c69ce19055aee728f3
-size 393
--- terraform.tfvars
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:41fd141d8190b14f128c4a24bf6ee7d09858529c3032eb87ac3b0663f4890a12
-size 19
